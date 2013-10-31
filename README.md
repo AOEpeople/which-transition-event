46780e2ced7b1d3589b40f9c7153d7b073ba0265
Sebastian Seilund <sebastian@seilund.com>
2013-10-31 14:11:37 -0700

    added code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # which-transition-event
+
+Kudos to http://stackoverflow.com/a/9090128/575896
---
+++ src/which-transition-event.js
@@ -0,0 +1,17 @@
+module.exports = function(){
+    var t;
+    var el = document.createElement('fakeelement');
+    var transitions = {
+        'transition':'transitionend',
+        'OTransition':'oTransitionEnd',
+        'MozTransition':'transitionend',
+        'WebkitTransition':'webkitTransitionEnd'
+    }
+
+    for(t in transitions){
+        if( el.style[t] !== undefined ){
+            return transitions[t];
+        }
+    }
+    return 'transitionend';
+}
