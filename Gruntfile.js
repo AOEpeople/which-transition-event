5fc530ba63ef5c978dcb2ab7a8679a8d80321462
Sebastian Seilund <sebastian@seilund.com>
2013-10-31 14:08:54 -0700

    added initial files

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        billy_builder: {
+            title: 'which-transition-event'
+        }
+    });
+
+    grunt.loadNpmTasks('billy-builder');
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# which-transition-event
